8de2e30220ef8ec6997ca5c25ee49de090cb2e9f
Evan Fox <evanigma@gmail.com>
2011-07-25 22:03:47 -0400

    Initial Front-End

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,22 +1,54 @@
-#include <iostream>
-#include <string>
 #include "MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
-//PRE: parent points to an initialized QWidget
-//POST: The application's GUI elements are created (menus, dock, toolbar), and we are set up to
-//		start playing songs
+MainWindow::MainWindow(QWidget* parent) : QWidget(parent)
 {
-	//setWindowIcon(QIcon(":/Icon/GLUI.png"));		//Set Icon to image as located by .qrc file
-	
-	setCentralWidget(this);						//every QMainWindow needs a central widget
-													//(the one that is the primary focus)
-														
-	/*connect(this, SIGNAL(newSong(Wave*)), 			//When we start playing a new song,
-			myPlayer, SLOT(playNewSong(Wave*)));	//let the SDL music player*/
-}
+    //setFixedSize(350,150);
+    QLabel* infoText = new QLabel("Kodak Motion Corder");
+    QLabel* fileLabel = new QLabel("Filename:");
+    QLineEdit* fileDialog = new QLineEdit();
+    
+    QSpinBox* startBox = new QSpinBox();
+    QLabel* startText = new QLabel("Start Frame:");
+    QSpinBox* endBox = new QSpinBox();
+    QLabel* endText = new QLabel("End Frame:");
+    
+    QCheckBox* vidCheck = new QCheckBox("Export Video");
+    QLabel* fpsText = new QLabel("fps:");
+    QSpinBox* fpsBox = new QSpinBox();
+    fpsBox->setMinimum(0);
+    
+    QPushButton* saveBtn = new QPushButton(tr("Save"), this);
 
-MainWindow::~MainWindow()
-//POST: Dynamically allocated memory not handled by Qt is freed.
-{
+    startBox->setRange(0,100);
+    endBox->setRange(0,100);
+    vidCheck->setCheckState(Qt::Checked);
+
+    connect(saveBtn, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    QFormLayout* layout = new QFormLayout;
+    QHBoxLayout* fileLayout = new QHBoxLayout;
+    fileLayout->addWidget(fileLabel);
+    fileLayout->addWidget(fileDialog);
+    
+    QHBoxLayout* frameBoxLayout = new QHBoxLayout;
+    frameBoxLayout->addWidget(startText);
+    frameBoxLayout->addWidget(startBox);
+    frameBoxLayout->addStretch();
+    frameBoxLayout->addWidget(endText);
+    frameBoxLayout->addWidget(endBox);
+    
+    layout->addRow(infoText);
+    layout->addRow(fileLayout);
+    layout->addRow(frameBoxLayout);
+    
+    QHBoxLayout* vidLayout = new QHBoxLayout;
+    vidLayout->addWidget(vidCheck);
+    vidLayout->addStretch();
+    vidLayout->addWidget(fpsText);
+    vidLayout->addWidget(fpsBox);
+    
+    
+    layout->addRow(vidLayout);
+    layout->addRow(saveBtn);
+    setLayout(layout);
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 // User Interface for Kodak MotionCorder Analyzer
 
 #include <QApplication>
+#include <QPushButton>
+#include <QWidget>
+#include <QObject>
 #include <iostream>
 #include "MainWindow.h"
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);               //application context which holds the program
-	MainWindow test(0);                         //the main user interface window (not yet visible)
-    
-	test.resize(300,400);                       //size the window to 300 pixels wide by 400 pixels high
-	test.show();                                //make window visible
+    MainWindow window;
+	window.show();                                //make window visible
+
 	return app.exec();                          //run the program!
 }
